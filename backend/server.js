62d77d10154512466ea35c3f383fbd622b822f96
Aman Mansuri <162823789+Mansuriamn@users.noreply.github.com>
2024-11-14 09:37:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+
 app.use(cors({
-  origin: ['https://identity-stkm.onrender.com', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+     origin: ['https://identity-stkm.onrender.com/', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+   }));
+app.use(express.json());
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
 
 // Database connection
